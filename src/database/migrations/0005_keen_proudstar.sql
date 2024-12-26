5f3259c6bd88ff628d0354208d72600baa512626
Thomas <thomasd.boch+github@gmail.com>
2024-12-26 15:29:55 +0100

    refactor: remove due_date and due_week columns from history table and update seeding logic

---
+++ src/database/migrations/0005_keen_proudstar.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `history` DROP COLUMN `due_date`;--> statement-breakpoint
+ALTER TABLE `history` DROP COLUMN `due_week`;
--- src/database/seed.ts
+++ src/database/seed.ts
@@ -1,10 +1,16 @@
+import { getCalendarWeekFromDateOfCurrentYear } from '../helper';
 import { db } from './db';
 import * as schema from './schema';
 
-export async function seedDatabase() {
-  const firstUser = await db.query.users.findFirst();
+const ENABLE_SEEDING = true;
 
-  if (!firstUser) {
+export async function seedDatabase() {
+  if (!ENABLE_SEEDING) {
+    console.log('Database Seeding is disabled.');
+    return;
+  }
+
+  if (!(await db.query.users.findFirst())) {
     await db.insert(schema.users).values([
       {
         username: 'john_doe',
@@ -21,7 +27,9 @@ export async function seedDatabase() {
         updated_at: new Date(),
       },
     ]);
+  }
 
+  if (!(await db.query.doings.findFirst())) {
     await db.insert(schema.doings).values([
       {
         name: 'Do the dishes',
@@ -36,30 +44,16 @@ export async function seedDatabase() {
         name: 'Take out the trash',
         description: 'Take the trash out to the curb',
         repetition: 'weekly',
-        effort_in_minutes: 10,
+        effort_in_minutes: 5,
+        days_per_week: 3,
         is_active: true,
         created_at: new Date(),
         updated_at: new Date(),
       },
     ]);
+  }
 
-    await db.insert(schema.assignments).values([
-      {
-        doing_id: 1,
-        user_id: 1,
-        status: 'pending',
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-      {
-        doing_id: 2,
-        user_id: 2,
-        status: 'completed',
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-    ]);
-
+  if (!(await db.query.shitty_points.findFirst())) {
     await db.insert(schema.shitty_points).values([
       {
         doing_id: 1,
@@ -76,11 +70,52 @@ export async function seedDatabase() {
         updated_at: new Date(),
       },
     ]);
+  }
 
-    await db.insert(schema.history).values([
+  if (!(await db.query.assignments.findFirst())) {
+    const currentWeek = getCalendarWeekFromDateOfCurrentYear(new Date());
+    await db.insert(schema.assignments).values([
       {
         doing_id: 1,
         user_id: 1,
+        status: 'pending',
+        due_week: currentWeek,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        doing_id: 2,
+        user_id: 2,
+        status: 'completed',
+        due_week: currentWeek,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        doing_id: 2,
+        user_id: 2,
+        status: 'pending',
+        due_week: currentWeek,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        doing_id: 2,
+        user_id: 2,
+        status: 'waiting',
+        due_week: currentWeek,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ]);
+  }
+
+  if (!(await db.query.history.findFirst())) {
+    await db.insert(schema.history).values([
+      {
+        doing_id: 1,
+        user_id: 2,
+        repetition: 'weekly',
         effort_in_minutes: 30,
         status: 'completed',
         created_at: new Date(),
@@ -88,14 +123,36 @@ export async function seedDatabase() {
       },
       {
         doing_id: 2,
-        user_id: 2,
+        user_id: 1,
+        repetition: 'daily',
+        days_per_week: 3,
+        effort_in_minutes: 10,
+        status: 'completed',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        doing_id: 2,
+        user_id: 1,
+        repetition: 'daily',
+        days_per_week: 3,
+        effort_in_minutes: 10,
+        status: 'completed',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        doing_id: 2,
+        user_id: 1,
+        repetition: 'daily',
+        days_per_week: 3,
         effort_in_minutes: 10,
         status: 'completed',
         created_at: new Date(),
         updated_at: new Date(),
       },
     ]);
-
-    console.log('Database Seeding complete.');
   }
+
+  console.log('Database Seeding complete.');
 }
