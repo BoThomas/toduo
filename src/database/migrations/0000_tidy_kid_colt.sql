28c1d307b4ba48c799f5573fb5dd63c7e2688536
Thomas <thomasd.boch+github@gmail.com>
2024-12-29 00:33:03 +0100

    init new db migration history

---
+++ src/database/migrations/0000_tidy_kid_colt.sql
@@ -0,0 +1,66 @@
+CREATE TABLE `assignments` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`doing_id` integer NOT NULL,
+	`user_id` integer NOT NULL,
+	`due_date` integer,
+	`due_week` integer,
+	`status` text NOT NULL,
+	`created_at` integer NOT NULL,
+	`updated_at` integer NOT NULL,
+	FOREIGN KEY (`doing_id`) REFERENCES `doings`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `doings` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL,
+	`description` text,
+	`notice` text,
+	`repetition` text NOT NULL,
+	`days_per_week` integer,
+	`effort_in_minutes` integer NOT NULL,
+	`is_active` integer,
+	`deleted_at` integer,
+	`created_at` integer NOT NULL,
+	`updated_at` integer NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `doings_name_unique` ON `doings` (`name`);--> statement-breakpoint
+CREATE TABLE `history` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`doing_id` integer NOT NULL,
+	`user_id` integer NOT NULL,
+	`repetition` text,
+	`days_per_week` integer,
+	`effort_in_minutes` integer,
+	`status` text,
+	`created_at` integer,
+	`updated_at` integer,
+	`history_date` integer,
+	FOREIGN KEY (`doing_id`) REFERENCES `doings`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `shitty_points` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`doing_id` integer NOT NULL,
+	`user_id` integer NOT NULL,
+	`points` integer NOT NULL,
+	`created_at` integer NOT NULL,
+	`updated_at` integer NOT NULL,
+	FOREIGN KEY (`doing_id`) REFERENCES `doings`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `users` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`username` text NOT NULL,
+	`auth0_id` text NOT NULL,
+	`participation_percent` integer DEFAULT 0 NOT NULL,
+	`deleted_at` integer,
+	`created_at` integer NOT NULL,
+	`updated_at` integer NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `users_username_unique` ON `users` (`username`);--> statement-breakpoint
+CREATE UNIQUE INDEX `users_auth0_id_unique` ON `users` (`auth0_id`);
